85ad38c6477a0c23f308b64ba3561c1b578d409e
Sanket <samtandon71@gmail.com>
2026-03-10 13:58:48 +0530

    fix railway port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // In-memory user storage
 const users = [];
